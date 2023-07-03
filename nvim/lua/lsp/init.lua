9c4f86da932796c3693bec34a6af3d1e45f1e7bd
cactusnix <cocalemontea@gmail.com>
2023-07-03 14:53:53 +0800

    feat: add lsp init config

---
+++ nvim/lua/lsp/init.lua
@@ -0,0 +1,15 @@
+local diagnostic_signs = {
+  {
+    name = "DiagnosticSignError",
+    text = "",
+  },
+  { name = "DiagnosticSignWarn", text = "" },
+  { name = "DiagnosticSignHint", text = "💡" },
+  { name = "DiagnosticSignInfo", text = "" },
+}
+for _, sign in ipairs(diagnostic_signs) do
+  vim.fn.sign_define(
+    sign.name,
+    { texthl = sign.name, text = sign.text, numhl = sign.name }
+  )
+end
