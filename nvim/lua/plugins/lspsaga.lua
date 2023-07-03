0885bf64d1531213c6943f1fbce79b80e6fc8047
cactusnix <cocalemontea@gmail.com>
2023-07-03 10:35:00 +0800

    feat: add keymap for lspsaga

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -5,6 +5,10 @@ return {
   },
   event = "LspAttach",
   config = function()
+    local keymap = vim.api.nvim_set_keymap
+    local opts = require("constants.options").KEYMAP_OPTS
+    keymap("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+    keymap("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
     require("lspsaga").setup()
   end,
 }
