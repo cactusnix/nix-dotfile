5961b0d214a041df7162e630dc4121541eb01c9d
cactusnix <cocalemontea@gmail.com>
2023-07-06 21:57:35 +0800

    feat: add lazygit terminal

--- nvim/lua/terminals/lazygit.lua
+++ nvim/lua/terminals/lazygit.lua
@@ -2,8 +2,14 @@ local opts = require("constants.options").KEYMAP_OPTS
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", direction = "float" })
 
-function _lazygit_toggle()
+---@diagnostic disable-next-line: lowercase-global
+function lazygit_terminal_toggle()
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>g", ":lua _lazygit_toggle()<CR>", opts)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>g",
+  ":lua lazygit_terminal_toggle()<CR>",
+  opts
+)
