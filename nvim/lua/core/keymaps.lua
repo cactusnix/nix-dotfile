115b13cb53abe85d9aa5d31bbd265a1a85e87530
cactusnix <cocalemontea@gmail.com>
2023-07-10 19:15:25 +0800

    fix: remove bad keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,13 +3,12 @@ vim.g.mapleader = " "
 local keymap = vim.api.nvim_set_keymap
 local opts = require("constants.options").KEYMAP_OPTS
 
-keymap("i", "ii", "<ESC>", opts)
-keymap("n", "qf", ":q!<CR>", opts)
 keymap("n", "<C-s>", ":w<CR>", opts)
-keymap("n", "qn", ":quitall<CR>", opts)
+
+-- Window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
-keymap("n", "<leader>lf", ":lua vim.lsp.buf.format()<CR>", opts)
+
+-- Plugins
