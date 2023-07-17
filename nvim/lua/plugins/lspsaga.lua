bd49bc5caaf4084f5c60715cf54f97162431f83c
cactusnix <cocalemontea@gmail.com>
2023-07-17 10:59:27 +0800

    feat: update lspsaga keymaps

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -5,6 +5,20 @@ return {
   },
   event = "LspAttach",
   config = function()
+    local keymap = vim.api.nvim_set_keymap
+    local get_keymap_opts = require("utils.opts").get_keymap_opts
+    keymap(
+      "n",
+      "gD",
+      "<cmd> Lspsaga goto_type_definition <CR>",
+      get_keymap_opts("Go to type definition")
+    )
+    keymap(
+      "n",
+      "gd",
+      "<cmd> Lspsaga goto_definition <CR>",
+      get_keymap_opts("Go to definition")
+    )
     require("lspsaga").setup()
   end,
 }
