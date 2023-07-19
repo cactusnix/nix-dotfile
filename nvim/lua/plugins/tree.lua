e42a9b7373e826ef3c2cefb1d4668b68f751da51
cactusnix <cocalemontea@gmail.com>
2023-07-19 20:02:01 +0800

    feat: update tree plugin config

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -1,6 +1,32 @@
+local icons = require("constants.icons")
+
 return {
   "nvim-tree/nvim-tree.lua",
-  config = function()
+  opts = {
+    view = {
+      hide_root_folder = true,
+    },
+    renderer = {
+      icons = {
+        git_placement = "after",
+      },
+    },
+    diagnostics = {
+      enable = true,
+      icons = {
+        hint = icons.diagnostics.hint,
+        info = icons.diagnostics.info,
+        warning = icons.diagnostics.warn,
+        error = icons.diagnostics.error,
+      },
+    },
+    git = {
+      enable = true,
+      show_on_dirs = false,
+      show_on_open_dirs = false,
+    },
+  },
+  config = function(_, opts)
     local keymap = vim.api.nvim_set_keymap
     local get_keymap_opts = require("utils.opts").get_keymap_opts
     keymap(
@@ -15,6 +41,6 @@ return {
       "<cmd> NvimTreeFocus <CR>",
       get_keymap_opts("Foucs nvim tree")
     )
-    require("nvim-tree").setup()
+    require("nvim-tree").setup(opts)
   end,
 }
