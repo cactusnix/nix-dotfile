21c1a6b45e3fd8c62ee7636511574835d7639b31
cactusnix <cocalemontea@gmail.com>
2023-07-21 10:27:52 +0800

    feat: 45% rewrite lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,13 +1,11 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
+    "folke/neodev.nvim",
     {
       "williamboman/mason.nvim",
       build = ":MasonUpdate",
     },
-    "williamboman/mason-lspconfig.nvim",
-    "jay-babu/mason-null-ls.nvim",
-    "jay-babu/mason-nvim-dap.nvim",
   },
   opts = {
     mason = {
@@ -23,14 +21,14 @@ return {
       {
         name = "lua",
         lsp = "lua_ls",
-        server = "lua_ls",
+        server = "lua-language-server",
         format = "stylua",
         config = require("lsp.lua"),
       },
       {
         name = "swift",
         lsp = "sourcekit",
-        server = "sourcekit",
+        server = "sourcekit-lsp",
         format = "swift_format",
         lint = "swiftlint",
         config = require("lsp.swift"),
@@ -38,34 +36,20 @@ return {
       {
         name = "json",
         lsp = "jsonls",
-        server = "jsonls",
+        server = "json-lsp",
         format = "prettier",
       },
     },
   },
   config = function(_, opts)
+    require("neodev").setup()
     local lsp_configs = require("lspconfig")
-    local mason_registry = require("mason-registry")
-    local servers = {}
-    local formats = {}
-    local lints = {}
-    for i, it in ipairs(opts.languages) do
-      formats[i] = it["format"]
-      lints[i] = it["lint"]
-      -- if mason_registry.has_package("swift") then
-      --   servers[i] = it["server"]
-      -- end
+    for _, it in ipairs(opts.languages) do
       -- TODO use better way to do this
       if lsp_configs[it["lsp"]] then
         lsp_configs[it["lsp"]].setup(it["config"] and it["config"] or {})
       end
     end
-    print(mason_registry.has_package("lua-language-server"))
-    -- for i, value in ipairs(mason_registry.get_all_packages()) do
-    --   print(value)
-    -- end
     require("mason").setup(opts.mason)
-    require("mason-lspconfig").setup(servers)
-    require("mason-null-ls").setup()
   end,
 }
