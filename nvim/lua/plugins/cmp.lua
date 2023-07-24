e9b36b843d6861da8d6f76a78e5f510552afd3b9
cactusnix <cocalemontea@gmail.com>
2023-07-24 17:16:49 +0800

    chore: make cmp better

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,50 +1,65 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
-    "hrsh7th/vim-vsnip",
-    "hrsh7th/cmp-vsnip",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+    {
+      "L3MON4D3/LuaSnip",
+      build = "make install_jsregexp",
+    },
+    "rafamadriz/friendly-snippets",
   },
-  event = "VimEnter",
+  event = "InsertEnter",
   config = function()
     local cmp = require("cmp")
-
     cmp.setup({
       snippet = {
         expand = function(args)
-          vim.fn["vsnip#anonymous"](args.body)
+          require("luasnip").lsp_expand(args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<C-n>"] = cmp.mapping.select_next_item({
+          behavior = cmp.SelectBehavior.Insert,
+        }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({
+          behavior = cmp.SelectBehavior.Insert,
+        }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }),
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "vsnip" },
-      }, {
+        { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
       }),
+      experimental = {
+        ghost_text = true,
+      },
     })
-
-    cmp.setup.cmdline("/", {
+    -- Search Cmp
+    cmp.setup.cmdline({ "/", "?" }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
         { name = "buffer" },
       },
     })
-
+    -- Cmd Cmp
     cmp.setup.cmdline(":", {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        { name = "cmdline" },
+        { name = "path", name = "cmdline" },
       }),
     })
   end,
