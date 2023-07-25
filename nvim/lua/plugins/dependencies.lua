7677ff540e764329f64089ed812999e4383b88d1
cactusnix <cocalemontea@gmail.com>
2023-07-25 17:32:28 +0800

    chore: move treesitter plugin

--- nvim/lua/plugins/dependencies.lua
+++ nvim/lua/plugins/dependencies.lua
@@ -1,5 +1,26 @@
 return {
   { "nvim-tree/nvim-web-devicons", lazy = true },
-  { "nvim-lua/plenary.nvim", lazy = true },
-  { "MunifTanjim/nui.nvim", lazy = true },
+  { "nvim-lua/plenary.nvim",       lazy = true },
+  { "MunifTanjim/nui.nvim",        lazy = true },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+        "swift",
+        "json",
+      },
+      sync_install = true,
+      auto_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    },
+  },
 }
