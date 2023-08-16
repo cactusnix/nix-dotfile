77d10fdd4d2e4d8277532c066be08b5ac5609e2a
cactusnix <cocalemontea@gmail.com>
2023-08-16 09:02:09 +0800

    feat: add all lsp client for it

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -81,7 +81,8 @@ return {
       local lualine_y = {
         {
           function()
-            local msg = "No Active Lsp"
+            local default_msg = "No Active Lsp"
+            local msg = default_msg
             local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
             local clients = vim.lsp.get_active_clients()
             if next(clients) == nil then
@@ -90,7 +91,11 @@ return {
             for _, client in ipairs(clients) do
               local filetypes = client.config.filetypes
               if filetypes and vim.tbl_contains(filetypes, buf_ft) then
-                return client.name
+                if msg == default_msg then
+                  msg = client.name
+                else
+                  msg = msg .. " | " .. client.name
+                end
               end
             end
             return msg
