3a2ba8a2e66b964a44901e0b2d85d394a4994e22
cactusnix <cocalemontea@gmail.com>
2024-01-04 17:24:54 +0800

    feat: add startup better escape dashboard three plugins

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -10,4 +10,24 @@ return {
       require("oil").setup()
     end
   },
+  -- The plugin can make jk to escape better.
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup()
+    end
+  },
+  -- The plugin can look startup time.
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime"
+  },
+  -- The plugin makes start screen beautiful.
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      require("dashboard").setup()
+    end
+  }
 }
