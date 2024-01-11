308a764777bed415441c4925a4eebcaa3d9527d7
cactusnix <cocalemontea@gmail.com>
2024-01-11 11:51:09 +0800

    feat: add cmp feature

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    version = fasle, -- For repo because last release is way too old.
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip", -- Bridge for LuaSnip
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    event = "InsertEnter",
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        completion = {
+          completionopt = "menu,menuone,noselect",
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- Use LuaSnip engine.
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["C-n"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["C-p"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["C-b"] = cmp.mapping.scroll_docs(-4),
+          ["C-f"] = cmp.mapping.scroll_docs(4),
+          ["C-e"] = cmp.mapping.abort(),
+        }),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
+      }
+    end,
+    config = function(_, opts)
+      require("cmp").setup(opts)
+    end,
+  },
+  -- The plugin is cmp engine.
+  {
+    "L3MON4D3/LuaSnip",
+  },
+}
