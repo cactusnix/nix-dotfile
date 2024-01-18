64f72fc896e4efc81ea5315f5141e997291a7a36
cactusnix <cocalemontea@gmail.com>
2024-01-18 18:27:38 +0800

    feat: add number feature

--- nvim/lua/settings/general.lua
+++ nvim/lua/settings/general.lua
@@ -4,6 +4,9 @@ opt.tabstop = 2 -- Replace tab with two spaces.
 opt.shiftwidth = 2 -- Indent two spaces.
 opt.expandtab = true -- Convert tab to space.
 
+opt.number = true -- Show line number.
+opt.relativenumber = true -- Show relative number.
+
 opt.smartcase = true -- Search ignore case.
 opt.ignorecase = true -- Ignore all case.
 
