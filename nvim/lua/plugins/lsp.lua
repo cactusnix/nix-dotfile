280221c7af2d4b8f412f177e4c6e032ca128d21b
cactusnix <cocalemontea@gmail.com>
2024-01-22 11:40:21 +0800

    fix: fix lsp attach watch many times

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -22,70 +22,70 @@ return {
       require("fidget").setup(opts.fidget)
       require("neodev").setup()
       require("lspsaga").setup(opts.lspsaga)
+      -- Watch lsp attach event to set keymap.
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function()
+          local keymap_set = require("utils").keymap_set
+          keymap_set(
+            "n",
+            "<leader>cl",
+            "<CMD>LspInfo<CR>",
+            { desc = "[C]urrent [L]sp info" }
+          )
+          keymap_set(
+            "n",
+            "K",
+            "<CMD>Lspsaga hover_doc<CR>",
+            { desc = "Hover Documentation" }
+          )
+          keymap_set(
+            "n",
+            "gr",
+            "<CMD>Telescope lsp_references<CR>",
+            { desc = "[g]o to [r]eferences" }
+          )
+          keymap_set(
+            "n",
+            "gd",
+            "<CMD>Telescope lsp_definitions<CR>",
+            { desc = "[g]o to [d]efinitions" }
+          )
+          keymap_set(
+            "n",
+            "gD",
+            vim.lsp.buf.declaration,
+            { desc = "[g]o to [D]eclaration" }
+          )
+          keymap_set(
+            "n",
+            "gI",
+            "<CMD>Telescope lsp_implementations<CR>",
+            { desc = "[g]o to [I]mplementations" }
+          )
+          keymap_set(
+            "n",
+            "gy",
+            "<CMD>Telescope lsp_type_definitions<CR>",
+            { desc = "[g]o to t[y]pe definitions" }
+          )
+          keymap_set(
+            "n",
+            "<leader>ca",
+            "<CMD>Lspsaga code_action<CR>",
+            { desc = "[c]ode [a]ction" }
+          )
+          keymap_set(
+            "n",
+            "<leader>cr",
+            "<CMD>Lspsaga rename<CR>",
+            { desc = "[c]ode [r]ename" }
+          )
+        end,
+      })
       for _, server in ipairs(opts.servers) do
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
         require("lspconfig")[server].setup({
           capabilities = capabilities,
-        })
-        -- Watch lsp attach event to set keymap.
-        vim.api.nvim_create_autocmd("LspAttach", {
-          callback = function()
-            local keymap_set = require("utils").keymap_set
-            keymap_set(
-              "n",
-              "<leader>cl",
-              "<CMD>LspInfo<CR>",
-              { desc = "[C]urrent [L]sp info" }
-            )
-            keymap_set(
-              "n",
-              "K",
-              "<CMD>Lspsaga hover_doc<CR>",
-              { desc = "Hover Documentation" }
-            )
-            keymap_set(
-              "n",
-              "gr",
-              "<CMD>Telescope lsp_references<CR>",
-              { desc = "[g]o to [r]eferences" }
-            )
-            keymap_set(
-              "n",
-              "gd",
-              "<CMD>Telescope lsp_definitions<CR>",
-              { desc = "[g]o to [d]efinitions" }
-            )
-            keymap_set(
-              "n",
-              "gD",
-              vim.lsp.buf.declaration,
-              { desc = "[g]o to [D]eclaration" }
-            )
-            keymap_set(
-              "n",
-              "gI",
-              "<CMD>Telescope lsp_implementations<CR>",
-              { desc = "[g]o to [I]mplementations" }
-            )
-            keymap_set(
-              "n",
-              "gy",
-              "<CMD>Telescope lsp_type_definitions<CR>",
-              { desc = "[g]o to t[y]pe definitions" }
-            )
-            keymap_set(
-              "n",
-              "<leader>ca",
-              "<CMD>Lspsaga code_action<CR>",
-              { desc = "[c]ode [a]ction" }
-            )
-            keymap_set(
-              "n",
-              "<leader>cr",
-              "<CMD>Lspsaga rename<CR>",
-              { desc = "[c]ode [r]ename" }
-            )
-          end,
         })
       end
     end,
