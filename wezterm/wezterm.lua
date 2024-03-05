06e30121ea5a40c7828672affe754ef29d6bb94a
cactusnix <cocalemontea@gmail.com>
2024-03-05 14:31:59 +0800

    feat: add feature for wezterm & update nvim plugins

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,9 +8,16 @@ config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 
 -- Font
-config.font_size = 15
+config.font = wezterm.font_with_fallback({
+  { family = "Fira Mono", weight = "Regular" },
+  { family = "PingFang SC", weight = "Regular" },
+  "Apple Color Emoji",
+})
+config.font_size = 16
+config.line_height = 1.2
 
 -- Window
 config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
 
 return config
