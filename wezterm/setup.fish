d6d400f0eab4c18ed1e5e84019d27eb03bec9c18
cactusnix <cocalemontea@gmail.com>
2024-09-04 10:40:23 +0800

    feat: add exit status for wezterm

--- wezterm/setup.fish
+++ wezterm/setup.fish
@@ -1,6 +1,6 @@
 # This shell is just for macOS.
 
-function check_homebrew
+function brew_check
   echo "Check hombrew..."
   if type brew -q
     return 0
@@ -12,7 +12,7 @@ end
 
 function setup
   echo "WezTerm setup start..."
-  if check_homebrew
+  if brew_check
     set success "WezTerm setup has been ready for use, just enjoy it." 
     set font font-fira-mono
     set packages $(brew list)
@@ -25,8 +25,11 @@ function setup
         echo $success
       else 
         echo "Font that font-fira-mono is not successfully installed "
+        exit 1
       end
     end
+  else
+    exit 1
   end
 end
 
