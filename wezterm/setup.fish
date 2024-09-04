3869268b3383dea115d9ac90f70d0ada7111956b
cactusnix <cocalemontea@gmail.com>
2024-09-04 09:11:01 +0800

    feat: add wezterm setup shell script

--- wezterm/setup.fish
+++ wezterm/setup.fish
@@ -1,5 +1,33 @@
-if type brew -q 
-  echo "Homebrew is installed"
-else
-  echo "Please install homebrew first"
+# This shell is just for macOS.
+
+function check_homebrew
+  echo "Check hombrew..."
+  if type brew -q
+    return 0
+  else
+    echo "Homebrew is not installed, please install it first"
+    return 1
+  end
 end
+
+function setup
+  echo "WezTerm setup start..."
+  if check_homebrew
+    set success "WezTerm setup has been ready for use, just enjoy it." 
+    set font font-fira-mono
+    set packages $(brew list)
+    if contains $font $packages
+      echo "Font that font-fira-mono has been installed."
+      echo $success
+    else 
+      echo "Execute: brew install font-fira-mono"
+      if brew install font-fira-mono
+        echo $success
+      else 
+        echo "Font that font-fira-mono is not successfully installed "
+      end
+    end
+  end
+end
+
+setup
